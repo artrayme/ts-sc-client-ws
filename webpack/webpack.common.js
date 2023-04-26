6b68ee160fff2f6f568942867e33bb4cf73020b4
artrayme <artrayme@protonmail.com>
2023-04-26 18:57:31 +0300

    build: add esm, cmj and webpack configs

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,29 +1,36 @@
 const path = require("path");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: "none",
-  entry: {
-    app: path.resolve(process.cwd(), "src/index.ts"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [],
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  externalsPresets: { node: true },
-  output: {
-    filename: "sc.js",
-    path: path.resolve(process.cwd(), "build"),
-    libraryTarget: "umd",
-    library: "sc",
-    globalObject: "this",
-  },
+    mode: "none",
+    entry: {
+        app: path.resolve(process.cwd(), "src/index.ts"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [{
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: "tsconfig.esm.json"
+                    }
+                }],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [],
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    externals: [nodeExternals()],
+    externalsPresets: {node: true},
+    output: {
+        filename: "sc.js",
+        path: path.resolve(process.cwd(), "build"),
+        libraryTarget: "umd",
+        library: "sc",
+        globalObject: "this",
+    },
 };
